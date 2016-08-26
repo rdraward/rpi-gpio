447a055a14f80eaa69af01a0eb471ec730cfa372
rdraward <rdraward@gmail.com>
2016-08-25 21:28:51 -0400

    joystick is working!

--- simple_joystick_input.py
+++ simple_joystick_input.py
@@ -1,19 +1,67 @@
+#!/usr/bin/python
+#--------------------------------------
+# This script reads data from a
+# MCP3008 ADC device using the SPI bus.
+#
+# Analogue joystick version!
+#
+# Author : Matt Hawkins
+# Date   : 17/04/2014
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
+ 
+import spidev
+import time
+import os
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
-GPIO.setup(33, GPIO.IN)
-GPIO.setup(35, GPIO.IN)
-GPIO.setup(37, GPIO.IN)
-try:
-	while True:
-		if GPIO.input(35) > 0:
-			GPIO.output(7, True)
-			#print(GPIO.input(35))
-		else:
-			GPIO.output(7, False)
 
-	#GPIO.cleanup()
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+ 
+# Function to read SPI data from MCP3008 chip
+# Channel must be an integer 0-7
+def ReadChannel(channel):
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
+ 
+# Define sensor channels
+# (channels 3 to 7 unused)
+swt_channel = 0
+vrx_channel = 1
+vry_channel = 2
+ 
+# Define delay between readings (s)
+delay = 0.5
+try: 
+  while True:
+ 
+    # Read the joystick position data
+    vrx_pos = ReadChannel(vrx_channel)
+    vry_pos = ReadChannel(vry_channel)
+ 
+    # Read switch state
+    swt_val = ReadChannel(swt_channel)
+ 
+    # Print out results
+    print "--------------------------------------------"
+    print("X : {}  Y : {}  Switch : {}".format(vrx_pos,vry_pos,swt_val))
+ 
+    # Connect LED to selection   
+    if swt_val < 1010:
+      GPIO.output(7, True)
+    else:
+      GPIO.output(7, False)
+
+    # Wait before repeating loop
+    time.sleep(delay)
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
+   GPIO.cleanup()
+
